4a0abb0612bc680e3637d79bfc5891da45a0a6ef
jerome crevoisier <crevoisierj@hotmail.com>
2024-10-30 10:14:58 +0100

    debug

--- main.py
+++ main.py
@@ -37,15 +37,16 @@ def get_credentials():
         # Create a dictionary with the service account info
         service_account_info = {
             "type": "service_account",
-            "project_id": os.environ.get("GOOGLE_PROJECT_ID"),
-            "private_key_id": os.environ.get("GOOGLE_PRIVATE_KEY_ID"),
-            "private_key": os.environ.get("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
-            "client_email": os.environ.get("GOOGLE_CLIENT_EMAIL"),
-            "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+            "project_id": os.environ.get("bachatanow"),
+            "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+            "private_key": os.environ.get("PRIVATE_KEY").replace('\\n', '\n'),
+            "client_email": os.environ.get("CLIENT_EMAIL"),
+            "client_id": os.environ.get("CLIENT_ID"),
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": os.environ.get("GOOGLE_CLIENT_X509_CERT_URL")
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gmail-api-access%40bachatanow.iam.gserviceaccount.com",
+            "universe_domain": "googleapis.com"
         }
 
         creds = service_account.Credentials.from_service_account_info(
