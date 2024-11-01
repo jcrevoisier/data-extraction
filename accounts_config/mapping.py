3be73745cebfdff7b85923d3ff58f6a7ac167a00
jerome crevoisier <crevoisierj@hotmail.com>
2024-11-01 10:16:15 +0100

    using instagram api

--- accounts_config/mapping.py
+++ accounts_config/mapping.py
@@ -1,5 +1,5 @@
 """Mapping."""
 
 Mapping = {
-    'workbn92': {'username': 'workbn92'}
+    'workbn92': {'username': 'workbn92', 'instagram_id' : '51470221531'}
 }
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import instaloader
 import time
 import pickle
 import os.path
@@ -8,7 +7,6 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from email.mime.text import MIMEText
 import base64
-from instaloader.exceptions import LoginRequiredException
 from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
@@ -17,6 +15,7 @@ from google.auth.transport.requests import Request
 from accounts_config.mapping import Mapping
 import logging
 from dotenv import load_dotenv
+import requests
 
 # Load environment variables
 load_dotenv()
@@ -25,12 +24,12 @@ load_dotenv()
 logging.basicConfig(filename='script.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Initialize Instaloader
-L = instaloader.Instaloader()
-
 # Add these at the beginning of your script
 INSTAGRAM_USERNAME = "workbn92"
 INSTAGRAM_PASSWORD = "Elenita_Es_La_Mas_Guapa_26102024"
+
+ACCESS_TOKEN = os.getenv('INSTAGRAM_ACCESS_TOKEN')
+INSTAGRAM_ACCOUNT_ID = os.getenv('INSTAGRAM_ACCOUNT_ID')
 
 # Email configuration
 EMAIL = "bachatanow.app@gmail.com"  # This email will be used for both sending and receiving alerts
@@ -38,6 +37,16 @@ EMAIL = "bachatanow.app@gmail.com"  # This email will be used for both sending a
 import os
 from google.oauth2 import service_account
 from google.auth import exceptions
+
+def get_instagram_account_info():
+    url = f"https://graph.instagram.com/v12.0/{INSTAGRAM_ACCOUNT_ID}?fields=id,username,account_type&access_token={ACCESS_TOKEN}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f'account info ok : {response.json()}')
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 
 def get_sheets_credentials():
     try:
@@ -120,18 +129,48 @@ def send_email(service, subject, body):
     except Exception as e:
         logging.info(f"Failed to send email: {e}")
 
-def check_new_post(account):
-    try:
-        if not L.context.is_logged_in:
-            L.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-        profile = instaloader.Profile.from_username(L.context, Mapping[account]['username'])
-        posts = profile.get_posts()
-        return next(posts)
-    except LoginRequiredException:
-        logging.info("Login required. Please check your Instagram credentials.")
+def get_instagram_business_account_id(username):
+    url = f"https://graph.instagram.com/v12.0/{INSTAGRAM_ACCOUNT_ID}"
+    params = {
+        "fields": "id,username",
+        "access_token": ACCESS_TOKEN
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json().get('id')
+    else:
+        print(f"Error: {response.text}")
         return None
-    except StopIteration:
-        return None
+
+def check_new_post(username):
+    url = f"https://graph.instagram.com/v12.0/{INSTAGRAM_ACCOUNT_ID}/media"
+    params = {
+        "fields": "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username",
+        "access_token": ACCESS_TOKEN,
+        "limit": 1
+    }
+
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and len(data['data']) > 0:
+            most_recent_post = data['data'][0]
+            return {
+                'id': most_recent_post['id'],
+                'caption': most_recent_post.get('caption', ''),
+                'media_type': most_recent_post['media_type'],
+                'media_url': most_recent_post.get('media_url', most_recent_post.get('thumbnail_url', '')),
+                'permalink': most_recent_post['permalink'],
+                'timestamp': most_recent_post['timestamp'],
+                'username': most_recent_post['username']
+            }
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+    return None
+
+    # except requests.exceptions.RequestException as e:
+    #     print(f"Error fetching posts for account {username}: {str(e)}")
+    #     return None
 
 def get_next_date(day):
     days = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
@@ -170,36 +209,39 @@ def main():
     SPREADSHEET_ID = '1u5dG4CU4bxm4pw3kEh3hZicql7ZG_XkWp7aTYddufTQ'  # Replace with your actual spreadsheet ID
 
     accounts = ['workbn92']
+    account_info = get_instagram_account_info()
+    print(f'account_info : {account_info}')
     try:
         for account in accounts:
             logging.info(f"Monitoring Instagram account: {account}")
+            print(f"Monitoring Instagram account: {account}")
             latest_post = check_new_post(account)
-
-            if latest_post and latest_post.shortcode != last_post_id:
-                logging.info(f"New post detected: {latest_post.url}")
-                logging.info(f"Post ID: {latest_post.shortcode}")
-
-                caption = latest_post.caption.lower() if latest_post.caption else ""
-                found_days = [day for day in days if day in caption]
-
-                if found_days:
-                    for day in found_days:
-                        next_date = get_next_date(day)
-                        logging.info(f"Next {day}: {next_date}")
-                        add_event_to_spreadsheet(sheets_service, SPREADSHEET_ID, day, next_date, latest_post.shortcode)
-
-                subject = f"New Instagram Post from {account}"
-                body = f"A new Instagram post has been detected for {account}.\n\nPost URL: {latest_post.url}\nPost ID: {latest_post.shortcode}"
-
-                if found_days:
-                    body += "\n\nUpcoming dates:"
-                    for day in found_days:
-                        next_date = get_next_date(day)
-                        body += f"\nNext {day}: {next_date}"
-
-                send_email(gmail_service, subject, body)
-
-                last_post_id = latest_post.shortcode
+            print(f'latest_post : {latest_post}')
+            # if latest_post and latest_post.shortcode != last_post_id:
+            #     logging.info(f"New post detected: {latest_post.url}")
+            #     logging.info(f"Post ID: {latest_post.shortcode}")
+            #
+            #     caption = latest_post.caption.lower() if latest_post.caption else ""
+            #     found_days = [day for day in days if day in caption]
+            #
+            #     if found_days:
+            #         for day in found_days:
+            #             next_date = get_next_date(day)
+            #             logging.info(f"Next {day}: {next_date}")
+            #             add_event_to_spreadsheet(sheets_service, SPREADSHEET_ID, day, next_date, latest_post.shortcode)
+            #
+            #     subject = f"New Instagram Post from {account}"
+            #     body = f"A new Instagram post has been detected for {account}.\n\nPost URL: {latest_post.url}\nPost ID: {latest_post.shortcode}"
+            #
+            #     if found_days:
+            #         body += "\n\nUpcoming dates:"
+            #         for day in found_days:
+            #             next_date = get_next_date(day)
+            #             body += f"\nNext {day}: {next_date}"
+            #
+            #     send_email(gmail_service, subject, body)
+            #
+            #     last_post_id = latest_post.shortcode
 
     except Exception as e:
         logging.info(f"An error occurred: {e}")
